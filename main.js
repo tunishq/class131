8d30f253c5c8129e76c545ed69b936b448248bca
tunishq <74109308+tunishq@users.noreply.github.com>
2022-01-23 10:57:54 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+img = "";
+
+function preload()
+{
+    img=loadImage("dog_cat.jpg");
+}
+
+function setup ()
+{
+canvas = createCanvas(640, 420);
+canvas.center();
+}
+
+function draw ()
+{
+    image(img, 0, 0, 640, 420);
+
+    fill("#ff0000");
+    text("dog", 45, 75);
+    noFill();
+    stroke("#ff0000");
+    rect(30, 60, 450, 340);
+}
